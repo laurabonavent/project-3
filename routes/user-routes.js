1366b6b410763358e4dfe3df5f6868f488b1949e
Laura Bonavent <laura.bonavent@gmail.com>
2020-11-19 19:10:19 +0100

    user route on going

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -5,7 +5,6 @@ const User = require("../models/User.model");
 const uploader = require("../configs/cloudinary-setup.config");
 
 // PUT SESSION
-
 userRouter.put("/user", uploader.single("avatar"), (req, res, next) => {
   const { email, password, username, level } = req.body;
   let avatarUser;
@@ -45,19 +44,38 @@ userRouter.put("/user", uploader.single("avatar"), (req, res, next) => {
     });
 });
 
-
 // POST FAVORIS
-// user findbyId and update + add l'id de la ressources 
+// user findbyId and update + add l'id de la ressources
 userRouter.post("/favorites", (req, res, next) => {
+  let ressourceId = req.params.id;
 
-})
-
-
+  User.findByIdAndDelete(req.session.id, ressourceId)
+    .then((user) => {
+      res.status(200).json({ favorites: ressourceId });
+    })
+    .catch((err) => {
+      res.status(400).json({ message: "Favorite not added" });
+    });
+});
 
 // GET USER PROFILE + FAVORIS
 userRouter.get("/user", (req, res, next) => {
-  
-})
+  if (!req.session.user) {
+    res.status(400).json({
+      message: "Please login before access the user profile",
+    });
+  }
+
+  User.findById(req.session.user.id)
+    .populate("favorites")
+    .then((user) => {
+      console.log(user.favorites);
+      res.status(200).json(user);
+    })
+    .catch((err) => {
+      res.status(400).json({ message: "No user found" });
+    });
+});
 // dans user + populate
 
 module.exports = userRouter;
