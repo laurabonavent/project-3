063524aa023a4b760621966f94a516ec56761f51
Nina Quaresma <quaresma.nina@gmail.com>
2020-11-19 21:03:21 +0100

    Switch + Components

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 
+import Login from "./components/Login";
+
 function App() {
   return (
     <div className='App'>
