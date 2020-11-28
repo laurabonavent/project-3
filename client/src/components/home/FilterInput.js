7d9a337f49ab74e849c5cc16736dcf9a0d56c1ac
Laura Bonavent <laura.bonavent@gmail.com>
2020-11-28 14:40:02 +0100

    ok

---
+++ client/src/components/home/FilterInput.js
@@ -0,0 +1,30 @@
+import "antd/dist/antd.css";
+import React from "react";
+import { Select } from "antd";
+const { Option } = Select;
+
+
+class FilterInput extends React.Component {
+  handleChange = (value) => {
+      console.log(`selected ${value}`);
+  };
+
+  render() {
+    return (
+      <>
+        <Select
+          mode="multiple"
+          allowClear
+          placeholder="Technology"
+          style={{ width: "100%" }}
+          onChange={this.handleChange}>
+          {this.props.dataFilters.map((el, index) => (
+            <Option key={index} value={el}>{el}</Option>
+          ))}
+        </Select>
+      </>
+    );
+  }
+}
+
+export default FilterInput;
---
+++ client/src/components/home/Filters.js
@@ -0,0 +1,29 @@
+import React from "react";
+import FilterInput from "./FilterInput";
+
+class Filters extends React.Component {
+  state = {
+  technology: ["all","api","browser","css","design","express","handlebars","html","javascript","mongodb",
+"nodejs",
+      "reactjs",
+      "vscode",
+    ],
+    type: ["article","blog","book","documentation","e-learning","extension","interview-preparation","library","people","tips","tool","training","tutorial","video",
+    ],
+    level: ["every force", "padawan", "jedi", "master jedi"],
+    price: ["free", "paid", "freemium"],
+    language: ["french", "english"],
+}
+
+  render() {
+
+    return (
+      <>
+        <FilterInput dataFilters={this.state.technology}/>
+        
+      </>
+    )
+  }
+}
+
+export default Filters;
