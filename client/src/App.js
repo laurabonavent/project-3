db929c60b836d60de8ed0e83b30e1acffe9e087b
Nina Quaresma <quaresma.nina@gmail.com>
2020-12-10 22:25:37 +0100

    Parallax ok App.js

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,15 @@ import Navbar from "./components/nav/Navbar";
 import Footer from "./components/nav/Footer";
 import BackButton from "./components/nav/BackButton";
 import EditProfile from "./components/profile/EditProfile";
-import Parallax from "./components/Parallax";
+
+import { Parallax, ParallaxLayer } from "react-spring/renderprops-addons";
+import DarkRed from "./images/dark-red.svg";
+import Yellow from "./images/yellow.svg";
+import OrangeRed from "./images/orange-red.svg";
+import Purple from "./images/purple.svg";
+import LightOrange from "./images/light-orange.svg";
+import LightPink from "./images/light-pink.svg";
+import Rocket from "./images/rocket.png";
 
 class App extends React.Component {
   state = { loggedInUser: null };
@@ -56,104 +64,216 @@ class App extends React.Component {
 
     return (
       <div className='App'>
-        {/* <div className='wrapper'>
-          <div className='parallax-container'>
-            <div className='background'>
-              <img className='vector1' src={Vector1} alt='' />
-              <img className='vector2' src={Vector2} alt='' />
-              <img className='vector3' src={Vector3} alt='' />
-            </div>
-            <div className='foreground'> */}
-        <Route
-          render={(props) => (
-            <>
-              <Navbar
-                userInSession={this.state.loggedInUser}
-                updateUser={this.updateLoggedInUser}
-                {...props}
-              />
+        <Parallax ref={(ref) => (this.parallax = ref)} pages={3}>
+          <ParallaxLayer
+            offset={0}
+            speed={0}
+            factor={3}
+            className='background'
+            style={{
+              //   backgroundImage: url("stars", true),
+              backgroundSize: "cover",
+            }}></ParallaxLayer>
 
-              <BackButton />
-              <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/parallax' component={Parallax} />
+          <ParallaxLayer
+            offset={0.5}
+            speed={0.5}
+            style={{ opacity: 15 }}
+            className='background'>
+            <img
+              src={DarkRed}
+              alt=''
+              style={{
+                display: "block",
+                width: "20%",
+                marginLeft: "70%",
+                marginTop: "-18%",
+              }}
+            />
+            <img
+              alt=''
+              src={LightPink}
+              style={{
+                display: "block",
+                width: "30%",
+                marginLeft: "20%",
+                marginTop: "-18%",
+              }}
+            />
+            <img
+              alt=''
+              src={Purple}
+              style={{
+                display: "block",
+                width: "20%",
+                marginLeft: "65%",
+                marginTop: "8%",
+              }}
+            />
+          </ParallaxLayer>
 
-                <Route
-                  exact
-                  path='/login'
-                  render={(props) => (
-                    <Login
-                      userInSession={this.state.loggedInUser}
-                      history={props.history}
-                      updateUser={this.updateLoggedInUser}
+          <ParallaxLayer offset={1} speed={0.2} style={{ opacity: 5 }}>
+            <img
+              alt=''
+              src={Yellow}
+              style={{ display: "block", width: "10%", marginLeft: "10%" }}
+            />
+            <img
+              alt=''
+              src={LightOrange}
+              style={{ display: "block", width: "20%", marginLeft: "75%" }}
+            />
+          </ParallaxLayer>
+
+          <ParallaxLayer offset={1.2} speed={0.4} style={{ opacity: 10 }}>
+            <img
+              src={OrangeRed}
+              alt=''
+              style={{
+                display: "block",
+                width: "70%",
+                marginLeft: "15%",
+                marginTop: "-18%",
+              }}
+            />
+          </ParallaxLayer>
+
+          <ParallaxLayer offset={1.5} speed={-0.3} style={{ opacity: 10 }}>
+            <img
+              alt=''
+              src={Purple}
+              className='purple'
+              style={{ display: "block", width: "100%", marginLeft: "0%" }}
+            />
+            <img
+              alt=''
+              src={LightOrange}
+              className='light-orange'
+              style={{ display: "block", width: "100%", marginLeft: "0%" }}
+            />
+            <img
+              alt=''
+              src={LightPink}
+              className='light-pink'
+              style={{ display: "block", width: "100%", marginLeft: "0%" }}
+            />
+          </ParallaxLayer>
+
+          <ParallaxLayer
+            offset={2.0}
+            speed={0}
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              pointerEvents: "none",
+            }}>
+            <img className='dark-red' src={DarkRed} alt='' />
+            <img className='yellow' src={Yellow} alt='' />
+            <img className='orange-red' src={OrangeRed} alt='' />
+          </ParallaxLayer>
+
+          <ParallaxLayer offset={0} speed={0} factor={1}>
+            <Route
+              render={(props) => (
+                <>
+                  <Navbar
+                    userInSession={this.state.loggedInUser}
+                    updateUser={this.updateLoggedInUser}
+                    {...props}
+                  />
+
+                  <BackButton />
+                  <Switch>
+                    <Route exact path='/' component={Home} />
+                    <Route exact path='/parallax' component={Parallax} />
+
+                    <Route
+                      exact
+                      path='/login'
+                      render={(props) => (
+                        <Login
+                          userInSession={this.state.loggedInUser}
+                          history={props.history}
+                          updateUser={this.updateLoggedInUser}
+                        />
+                      )}
                     />
-                  )}
-                />
-                <Route
-                  exact
-                  path='/signup'
-                  render={(props) => (
-                    <Signup
-                      history={props.history}
-                      updateUser={this.updateLoggedInUser}
+                    <Route
+                      exact
+                      path='/signup'
+                      render={(props) => (
+                        <Signup
+                          history={props.history}
+                          updateUser={this.updateLoggedInUser}
+                        />
+                      )}
                     />
-                  )}
-                />
-                <Route
-                  exact
-                  path='/profile/edit'
-                  render={(props) => (
-                    <EditProfile
-                      {...props}
-                      history={props.history}
-                      updateUser={this.updateLoggedInUser}
-                      userInSession={this.state.loggedInUser}
+                    <Route
+                      exact
+                      path='/profile/edit'
+                      render={(props) => (
+                        <EditProfile
+                          {...props}
+                          history={props.history}
+                          updateUser={this.updateLoggedInUser}
+                          userInSession={this.state.loggedInUser}
+                        />
+                      )}
                     />
-                  )}
-                />
-                <Route
-                  exact
-                  path='/profile'
-                  render={(props) => (
-                    <Profile
-                      {...props}
-                      userInSession={this.state.loggedInUser}
+                    <Route
+                      exact
+                      path='/profile'
+                      render={(props) => (
+                        <Profile
+                          {...props}
+                          userInSession={this.state.loggedInUser}
+                        />
+                      )}
                     />
-                  )}
-                />
-                <Route
-                  exact
-                  path='/ressources/create'
-                  component={CreateRessource}
-                />
-                <Route
-                  exact
-                  path='/ressources/edit/:id'
-                  render={(props) => (
-                    <EditRessource
-                      {...props}
-                      userInSession={this.state.loggedInUser}
+                    <Route
+                      exact
+                      path='/ressources/create'
+                      component={CreateRessource}
                     />
-                  )}
-                />
-                <Route
-                  exact
-                  path='/ressources/:id'
-                  render={(props) => (
-                    <Ressource
-                      {...props}
-                      updateUser={this.updateLoggedInUser}
-                      userInSession={this.state.loggedInUser}
+                    <Route
+                      exact
+                      path='/ressources/edit/:id'
+                      render={(props) => (
+                        <EditRessource
+                          {...props}
+                          userInSession={this.state.loggedInUser}
+                        />
+                      )}
                     />
-                  )}
-                />
-              </Switch>
-              <Footer userInSession={this.state.loggedInUser} />
-            </>
-          )}></Route>
-        {/* </div>
-          </div>
-        </div> */}
+                    <Route
+                      exact
+                      path='/ressources/:id'
+                      render={(props) => (
+                        <Ressource
+                          {...props}
+                          updateUser={this.updateLoggedInUser}
+                          userInSession={this.state.loggedInUser}
+                        />
+                      )}
+                    />
+                  </Switch>
+                  <Footer userInSession={this.state.loggedInUser} />
+                </>
+              )}></Route>
+          </ParallaxLayer>
+          <ParallaxLayer
+            offset={2}
+            speed={-0.5}
+            style={{
+              display: "flex",
+              alignItems: "flex-end",
+              justifyContent: "flex-end",
+              margin: "-4% 0% 0% -3%",
+            }}>
+            <img alt='' src={Rocket} style={{ width: "10%" }} />
+          </ParallaxLayer>
+        </Parallax>
       </div>
     );
   }
