873b9d758255cf925679af12d91bb08d59e8286a
Laura Bonavent <laura.bonavent@gmail.com>
2020-12-12 14:46:56 +0100

    ok

--- client/src/components/nav/Buttons.js
+++ client/src/components/nav/Buttons.js
@@ -6,7 +6,7 @@ import { RiAccountPinCircleLine } from "react-icons/ri";
 
 const LoginButton = () => {
   return (
-    <Link to='/login'>
+    <Link to="/login">
       <RiAccountPinCircleLine />
     </Link>
   );
@@ -15,27 +15,27 @@ const LoginButton = () => {
 export { LoginButton };
 
 const SignupButton = () => {
-  return <Link to='/signup'>Sign up</Link>;
+  return <Link to="/signup">Sign up</Link>;
 };
 
 export { SignupButton };
 
 const LogoutButton = (props) => {
-  return <Link {...props}>Log out</Link>;
+  return <Button {...props}>Log out</Button>;
 };
 
 export { LogoutButton };
 
 const ProfileButton = () => {
-  return <Link to='/profile'>Profile</Link>;
+  return <Link to="/profile">Profile</Link>;
 };
 
 export { ProfileButton };
 
 const SuggestRessource = () => {
   return (
-    <Link to='/'>
-      <Button type='primary' shape='round'>
+    <Link to="/">
+      <Button type="primary" shape="round">
         Suggest a ressource
       </Button>
     </Link>
